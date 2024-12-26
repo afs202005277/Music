4f4528be9e8507c687d6fa41f3bbdbf08e72c103
andre <up202005277@up.pt>
2024-12-26 16:05:13 +0000

    ran dataset creation

--- create_dataset.py
+++ create_dataset.py
@@ -382,6 +382,13 @@ def main(start_date_str, duration_years):
         df = join_dataframes_vertically(df, matched)
         df = df.drop('Artist', axis=1)
         df = df.rename(columns={'playlist_genre': 'genre'})
+
+        cols_to_fix = ['danceability', 'energy', 'speechiness', 'acousticness', 'speechiness', 'valence']
+
+        for col in cols_to_fix:
+            df[col] = df[col].apply(lambda x: x / 100 if x > 1 else x)
+
+
         df.to_csv(final_hits_dataset_file, index=False)
 
         songs_to_add_non_hits = get_songs_to_add(df, million_songs_dataset)
